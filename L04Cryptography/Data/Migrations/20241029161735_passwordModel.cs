b32197b850d9673257734f3a3fa3f2a43b7c3ea7
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2024-10-29 12:40:23 -0400

    Cryptography - Part 1

---
+++ L04Cryptography/Data/Migrations/20241029161735_passwordModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace L04Cryptography.Data.Migrations
+{
+    public partial class passwordModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Password",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlainTextPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HashedPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HashedSaltedPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BcryptPassword = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Password", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Password");
+        }
+    }
+}
